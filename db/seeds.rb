c8a9a4c5966faaa2ad73ffdcabacadf82c72634e
William Mtemi Mponzi <willbrian009@gmail.com>
2023-06-21 11:15:13 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: "Will", last_name: "Stone", grade: 90)
